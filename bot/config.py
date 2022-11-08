cc7e98b7ffa9a5d65617fe15ef9c387c010fe76e
alpha-alexxx <65218056+alpha-alexxx@users.noreply.github.com>
2022-11-08 18:24:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,23 +3,23 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("1922128579:AAGehwWzSN2l9Wqyr55uQ-Lb82kbfT33FEM")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-    SESSION_NAME = os.environ.get("Lethargic", "utubeitbot")
+    SESSION_NAME = ":memory:"
 
-    API_ID = int(os.environ.get("7974446"))
+    API_ID = int(os.environ.get("API_ID"))
 
-    API_HASH = os.environ.get("049ce8d53b3d6198e17e378b52830909")
+    API_HASH = os.environ.get("API_HASH")
 
-    CLIENT_ID = os.environ.get("72100313729-4e8f6mfmn2qjim8pd8p1qomro54sd3ul.apps.googleusercontent.com")
+    CLIENT_ID = os.environ.get("CLIENT_ID")
 
-    CLIENT_SECRET = os.environ.get("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")
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-    BOT_OWNER = int(os.environ.get("688515695688515695"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 
-    AUTH_USERS_TEXT = os.environ.get("688515695", "")
+    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
-    AUTH_USERS = [BOT_OWNER, 688515695] + (
+    AUTH_USERS = [BOT_OWNER, 754495556] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
@@ -30,7 +30,7 @@ class Config:
     )
 
     VIDEO_CATEGORY = (
-        int(os.environ.get("27")) if os.environ.get("VIDEO_CATEGORY") else 0
+        int(os.environ.get("VIDEO_CATEGORY")) if os.environ.get("VIDEO_CATEGORY") else 0
     )
 
     VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "")
@@ -39,7 +39,7 @@ class Config:
 
     DEBUG = bool(os.environ.get("DEBUG"))
 
-    UPLOAD_MODE = os.environ.get("unlisted") or False
+    UPLOAD_MODE = os.environ.get("UPLOAD_MODE") or False
     if UPLOAD_MODE:
         if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
             UPLOAD_MODE = UPLOAD_MODE.lower()
