e37974e7aeacd360310b7c94dd98eaa7f89f262f
alpha-alexxx <65218056+alpha-alexxx@users.noreply.github.com>
2022-11-17 16:00:33 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -20,14 +20,7 @@ async def _start(c: UtubeBot, m: Message):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([
                   [
-                      InlineKeyboardButton("How To Use Me🙄", callback_data="/help")
-                  ],
-                  [
-                      InlineKeyboardButton("Project Channel!", url="https://t.me/hxbots"),
-                      InlineKeyboardButton("Support Group", url="https://t.me/HxSupport")
-                  ],
-                  [  
-                      InlineKeyboardButton("Buy Me A Tea", url="https://upayme.vercel.app/kkirodewal@ybl")
+                      InlineKeyboardButton("Visit My Website", url="https://blogbleat.info")
                   ]]
         ),
     )
